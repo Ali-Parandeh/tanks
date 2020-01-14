eb137e7c68087fd488a1f4db72e7cc325286d830
Ali Parandeh <Ali.Parandeh@outlook.com>
2020-01-14 00:43:45 +0000

    Initial check-in of module Tanks

---
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;         
+    public float m_Speed = 12f;            
+    public float m_TurnSpeed = 180f;       
+    public AudioSource m_MovementAudio;    
+    public AudioClip m_EngineIdling;       
+    public AudioClip m_EngineDriving;      
+    public float m_PitchRange = 0.2f;
+
+    private string m_MovementAxisName;     
+    private string m_TurnAxisName;         
+    private Rigidbody m_Rigidbody;         
+    private float m_MovementInputValue;    
+    private float m_TurnInputValue;        
+    private float m_OriginalPitch;         
+
+    // As soon as the scene is awakened
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Use this method to disable tanks
+    private void OnEnable ()
+    {
+        // Tanks to be set inactive. When rigid bodies are set as kinematic, no forces are applied on them.
+        m_Rigidbody.isKinematic = false;
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+    // Use this method to enable tanks
+    private void OnDisable ()
+    {
+        m_Rigidbody.isKinematic = true;
+    }
+
+
+    private void Start()
+    {
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;
+        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+
+        m_OriginalPitch = m_MovementAudio.pitch;
+    }
+    
+
+    private void Update()
+    {
+        // Store the player's input and make sure the audio for the engine is playing.
+    }
+
+
+    private void EngineAudio()
+    {
+        // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Move and turn the tank.
+    }
+
+
+    private void Move()
+    {
+        // Adjust the position of the tank based on the player's input.
+    }
+
+
+    private void Turn()
+    {
+        // Adjust the rotation of the tank based on the player's input.
+    }
+}
