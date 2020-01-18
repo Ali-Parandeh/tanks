23701485ee9cfacc109d25d669c63255e6db4894
Ali Parandeh <Ali.Parandeh@outlook.com>
2020-01-18 11:15:12 +0000

    Add Shell Shooting Function

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 public class TankShooting : MonoBehaviour
 {
-    public int m_PlayerNumber = 1;       
+    public int m_PlayerNumber = 1;      
     public Rigidbody m_Shell;            
     public Transform m_FireTransform;    
     public Slider m_AimSlider;           
@@ -11,18 +11,22 @@ public class TankShooting : MonoBehaviour
     public AudioClip m_ChargingClip;     
     public AudioClip m_FireClip;         
     public float m_MinLaunchForce = 15f; 
-    public float m_MaxLaunchForce = 30f; 
+    public float m_MaxLaunchForce = 30f;
+    // How long it takes to get from min charge force to max charge force
     public float m_MaxChargeTime = 0.75f;
-
-    /*
-    private string m_FireButton;         
-    private float m_CurrentLaunchForce;  
-    private float m_ChargeSpeed;         
+    // Input buttons are strings
+    private string m_FireButton;
+    // Keep track of how much charge force we have built up
+    private float m_CurrentLaunchForce;
+    // How fast the launch force increases
+    private float m_ChargeSpeed;
+    // Know whether or not we have fired yet
     private bool m_Fired;                
 
 
     private void OnEnable()
     {
+        // Reset the charge and slider when not firing
         m_CurrentLaunchForce = m_MinLaunchForce;
         m_AimSlider.value = m_MinLaunchForce;
     }
@@ -31,19 +35,59 @@ public class TankShooting : MonoBehaviour
     private void Start()
     {
         m_FireButton = "Fire" + m_PlayerNumber;
-
+        // Distance covered over time
         m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
     }
-    */
-
+    
+    // Called every frame
     private void Update()
     {
         // Track the current state of the fire button and make decisions based on the current launch force.
+        m_AimSlider.value = m_MinLaunchForce;
+
+        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        {
+            // at max charge - not yet fired
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire();
+        }
+        else if (Input.GetButtonDown(m_FireButton))
+        {
+            // Have we pressed fire for the first time?
+            m_Fired = false;
+            m_CurrentLaunchForce = m_MinLaunchForce;
+
+            m_ShootingAudio.clip = m_ChargingClip;
+            m_ShootingAudio.Play();
+        }
+            // Holding the fire button, not yet fired?
+        else if (Input.GetButton(m_FireButton) && !m_Fired)
+        {
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+            // Update the size of arrow launch force slider
+            m_AimSlider.value = m_CurrentLaunchForce;
+        }
+        else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
+        {
+            // Have we released the button but not yet fired?
+            Fire();
+        }
     }
 
 
     private void Fire()
     {
         // Instantiate and launch the shell.
+        m_Fired = true;
+        // NOTE: 'as Rigidbody' syntax is required since Instantiate() will create the new object from prefab as an Object datatype only
+        Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+        // Set the shell velocity to current launch force multiplied with the forward direction vector of the fire Transform component
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+        // Automatically stops the current audio souce playing
+        m_ShootingAudio.clip = m_FireClip;
+        m_ShootingAudio.Play();
+
+        m_CurrentLaunchForce = m_MinLaunchForce;
+
     }
 }
